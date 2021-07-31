82c44c39487d379ec1e149775577ce37e1010c65
Nikhil Ratta <nikhilratta84@gmail.com>
2021-07-31 23:32:56 +0530

    Added nodemon module and some functionalities

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = 'NzkyMzY1MTYzMzU2ODgwODk4.X-cpkA.AgOQZi0ql7CUrgaqhLINvvhbMpc';
 
 bot.login(TOKEN);
 
@@ -10,14 +10,21 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
+  if (msg.content === '!hi') {
+    msg.reply('Hello User');
+    msg.channel.send('How are you?');
   } else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }
+  else if(msg.content.startsWith('!how')){
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`${taggedUser.username} is super adorable :)`);
     } else {
       msg.reply('Please tag a valid user!');
     }
